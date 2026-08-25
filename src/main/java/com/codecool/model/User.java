928c58771a1c28aeaa11822f559ec4601216d211
pz <pasztor.zzz@gmail.com>
2026-08-25 20:50:37 +0200

    change_User

--- src/main/java/com/codecool/model/User.java
+++ src/main/java/com/codecool/model/User.java
@@ -1,4 +1,33 @@
 package com.codecool.model;
 
-public record User(String name, String email, String password) {
+public class User {
+  private static final int USER_ID = 1;
+
+  private final int uniqueID;
+  private final String name;
+  private final String email;
+  private final String password;
+
+  public User(String name, String email, String password) {
+    this.uniqueID = USER_ID + 1;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  public int getUniqueID() {
+    return uniqueID;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
 }
