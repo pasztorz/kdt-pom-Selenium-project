d27f80594080198d174c92f34a23bccb9acdfb07
pz <pasztor.zzz@gmail.com>
2026-08-31 11:34:06 +0200

    add_HomePageTest_basis_with_openPageTest()

---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,32 @@
+import com.codecool.pages.Home;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePageTest {
+  private WebDriver driver;
+  private Home home;
+
+  @BeforeEach
+  void setUp() {
+    driver = new ChromeDriver();
+    home = new Home(driver);
+
+    driver.manage().window().maximize();
+  }
+
+  @Test
+  public void openHomeTest() {
+    home.openPage();
+
+    Assertions.assertTrue(home.isPageOpen("https://playground.qatools.dev"));
+  }
+
+  @AfterEach
+  void tearDown() {
+    driver.quit();
+  }
+}
